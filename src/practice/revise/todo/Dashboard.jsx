e4a35325a4ea11b37b31a11d9f0522ba3c71dd83
Aashish Ramtel <aaseshrl@gmail.com>
2026-06-15 00:02:42 +0545

    complete todo task and search task in progress

--- src/practice/revise/todo/Dashboard.jsx
+++ src/practice/revise/todo/Dashboard.jsx
@@ -1,78 +1,47 @@
-import {useState} from 'react';
-import { FaTrashAlt } from "react-icons/fa";
+import {useState, useEffect} from 'react';
 
 const Dashboard = () => {
-    // const [task, setTask] = useState("")
-    // let data = {
-    //     id: Date.now(),
-    //     name: task,
-    //     status: false
-    // }
-    
-    const [task, setTask] = useState("")
-    const [taskList, setTaskList] = useState([])
-    const [taskDone, setTaskDone] = useState(false)
-    
-    const handleTask = () =>{
-        const newTask = {
-            id: Date.now(),
-            status: false,
-            name: task
+
+    const [userData, setUserData] = useState([])
+    const [search, setSearch] = useState("")
+
+    useEffect(()=>{
+        const fetchUser = async () =>{
+            try{
+                const response = await fetch("https://jsonplaceholder.typicode.com/users");
+                if(!response.ok) {
+                    throw new Error("Fetch Failed")
+                }
+                const data = await response.json()
+                setUserData(data);
+            }catch(error){
+                alert(error.message)
+            }
         }
+        fetchUser();
+    },[])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
         
-        setTaskList(prev=>(
-            [newTask,...prev]
-        ))
-        setTask("")
+        if(search.trim()==="") return;
+        setUserData(prev=>
+            prev.filter(user=> user.name.toLowerCase().includes(search.toLowerCase()))
+        )
     }
-
-    const handleTaskChange = (e) => {
-        setTask({
-            id: Date.now(),
-            name: e.target.value
-        })
-    }
-
-    const handleTaskDelete = (e) => {
-        
-    }
-
-    const handleKeyDown = (e) => {
-        if(e.key==="enter"){
-            handleTask()
-        }
-    }
-
+    
     return (
-        <>
-            <div className="flex items-center gap-3">
-                <input value={task.name} onChange={handleTaskChange} type="text" placeholder="Type Text" className="p-1 px-2 h-8 rounded-sm"/>
-                <button onKeyDown={handleKeyDown} onClick={handleTask} className="h-8 p-1 px-4 text-white text-sm bg-emerald-800 rounded-sm border-0 cursor-pointer">
-                    Add Task
-                </button>
-            </div>
-            <div className="w-3xl mt-4 p-3 py-1.5 border border-emerald-400">
-                <h1 className='text-gray-400 text-sm'>Task List</h1>
-                {taskList.length===0?(
-                    <p className='my-4'>No Task At the Moment</p>
-                ):(
-                    taskList.map((i, index)=>(
-                        <div key={index} className='flex justify-between gap-2'>
-                            <div className='flex gap-2'>
-                                <input required={taskDone} onChange={()=>setTaskDone(prev=>!prev)} type="checkbox" /> 
-                                <p className={`${taskDone?"line-through":""}`}>{i.name}</p>
-                            </div>
-                            <button onClick={handleTaskDelete} className='border-0!' title="Delete Task">
-                                <FaTrashAlt/>
-                            </button>
-                        </div>
-                    ))
-                )}
-            </div>
-            <div className='mt-4 px-4 py-2 bg-amber-200'>
-                <p>Total Tasks: <span className='font-bold'>{taskList.length}</span></p>
-            </div>
-        </>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder='search user' value={search} onChange={(e)=>setSearch(e.target.value)}/>
+                <button type='submit'>Search</button>
+            </form>
+            
+            Hello World
+            {userData.map(i=>(
+                <p>{i.name}</p>
+            ))}
+        </div>
     )
 }
 
